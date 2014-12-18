72a940267cf3ca41acb740e0195693336c946ce9
thatknitter <kblewis.flt@gmail.com>
2014-12-18 09:17:38 -0600

    login untested

---
+++ js/constants.js
@@ -0,0 +1,6 @@
+;( function() {
+    'use strict';
+    angular.module('healthApp')
+      .constant('FIREBASE_URL', 'https://healthrecipes.firebaseio.com');
+
+  }());
--- js/controllers.js
+++ js/controllers.js
@@ -3,38 +3,117 @@
 
     angular.module('healthApp')
     
+    .factory('authFactory', function(FIREBASE_URL) {
+      var factory = {}, ref = new Firebase(FIREBASE_URL);
+
+      factory.login = function(email, pass, cb) {
+        ref.authWithPassword({
+          email : email,
+          password : pass
+        }, function(error, authData) {
+          if (error === null) {
+            console.log('User logged in successfully', authData);
+            cb();
+          } else {
+            console.log('Error logging in user:', error);
+          }
+        });
+      };
+
+      factory.logout = function(cb) {
+        ref.unauth(function() {
+          cb();
+        });
+      };
+
+      factory.register = function(email, pass, cb) {
+        ref.createUser({
+          email : email,
+          password : pass
+        }, function(error, authData) {
+          if (error === null) {
+            console.log('User created successfully', authData);
+            cb();
+          } else {
+            console.log('Error creating user:', error);
+          }
+        });
+      };
+
+      factory.resetPassword = function(email) {
+        ref.resetPassword({
+          email : email
+        }, function(error) {
+          if (error === null) {
+            console.log('Password reset email sent successfully');
+          } else {
+            console.log('Error sending password reset email:', error);
+          }
+        });
+      };
+
+      return factory;
+    })
+    
+    .controller('LoginController', function(authFactory, $scope, $location) {
+      var vm = this;
+
+      vm.login = function() {
+        authFactory.login(vm.email, vm.password, function() {
+          $location.path('/');
+          $scope.$apply();
+        });
+      };
+
+      vm.register = function() {
+        authFactory.register(vm.email, vm.password, function() {
+          vm.login();
+        });
+      };
+
+      vm.forgotPassword = function() {
+        authFactory.resetPassword(vm.email);
+      };
+    })
+    
+    .controller('LogoutController', function($scope, $location, authFactory) {
+      authFactory.logout(function() {
+        $location.path('/');
+        $scope.$apply();
+      });
+    })
+    
     .controller('FormAdder', function($http, $routeParams) {
       var vm = this;
       vm.recipes = [];
-      
-      $http.get('https://healthrecipes.firebaseio.com/')
-      .success(function(data){
+
+      $http.get('https://healthrecipes.firebaseio.com/').success(function(data) {
         vm.heathrecipes = data;
-      }).error(function(err){
+      }).error(function(err) {
         alert('There has been a problem connecting to the API.');
       });
-      
+
       var $quick = $('.quick');
       $quick.click(addQuick);
 
       var $equip = $('.equip');
       $equip.click(addEquip);
-      
+
       var $fresh = $('.fresh');
       $fresh.click(addFresh);
-      
+
       var $panFresh = $('.panFresh');
       $panFresh.click(addPanFresh);
-      
+
       var $panDry = $('.panDry');
       $panDry.click(addPanDry);
-      
+
       var $herbSpice = $('.herbSpice');
       $herbSpice.click(addHerb);
-      
+
       var $direction = $('.direction');
       $direction.click(addDirection);
-     
+
       var quickNum = 1;
       var equipNum = 1;
       var freshNum = 1;
@@ -42,124 +121,111 @@
       var panDryNum = 1;
       var herbNum = 1;
       var dirNum = 1;
-      
+
       function addQuick() {
         var $list = $('<li></li>');
         var title = 'Quick Item ' + quickNum;
         var $label = $('<label>' + title + '</label>');
         var $formItem = $('<input class="form-control" type="text" ng-model="recipe.newRecipe.quick' + quickNum + '">');
         var $target = $('.quickTarget');
-                
+
         $list.append($label);
         $list.append($formItem);
         $target.append($list);
         quickNum = quickNum + 1;
 
       }
-      
-      function addEquip(){
+
+      function addEquip() {
         var $list = $('<li></li>');
         var title = 'Equipment ' + equipNum;
         var $label = $('<label>' + title + '</label>');
         var $formItem = $('<input class="form-control" type="text" ng-model="recipe.newRecipe.equip' + equipNum + '">');
         var $target = $('.equipTarget');
-        
+
         $list.append($label);
         $list.append($formItem);
         $target.append($list);
         equipNum = equipNum + 1;
       }
-      
-      function addFresh(){
+
+      function addFresh() {
         var $list = $('<li></li>');
         var title = 'Fresh Ingredient ' + freshNum;
         var $label = $('<label>' + title + '</label>');
-        var $formItem = $('<input class="form-control" type="text" ng-model="recipe.newRecipe.fresh' + freshNum + '"></br>' + '<label>Amount</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.amount' + freshNum + '"></br>' + '<label>Ingredient Prep</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.prep' + freshNum + '"></br>' + '<label>Special Instructions</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.special' + freshNum + '">');
+        var $formItem = $('<input class="form-control" type="text" ng-model="recipe.newRecipe.fresh' + freshNum + '"></br>' + '<label>Amount</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.amount' + freshNum + '"></br>' + '<label>Ingredient Prep</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.prep' + freshNum + '"></br>' + '<label>Special Instructions</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.special' + freshNum + '">');
         var $target = $('.freshTarget');
-        
+
         $list.append($label);
         $list.append($formItem);
         $target.append($list);
         freshNum = freshNum + 1;
       }
-      
-      function addPanFresh(){
+
+      function addPanFresh() {
         var $list = $('<li></li>');
         var title = 'Pantry Fresh Ingredient ' + panFreshNum;
         var $label = $('<label>' + title + '</label>');
-        var $formItem = $('<input class="form-control" type="text" ng-model="recipe.newRecipe.panFresh' + panFreshNum + '"></br>' + '<label>Amount</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.panFamount' + panFreshNum + '"></br>' + '<label>Ingredient Prep</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.panFprep' + panFreshNum + '"></br>' + '<label>Special Instructions</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.panFspecial' + panFreshNum + '">');
+        var $formItem = $('<input class="form-control" type="text" ng-model="recipe.newRecipe.panFresh' + panFreshNum + '"></br>' + '<label>Amount</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.panFamount' + panFreshNum + '"></br>' + '<label>Ingredient Prep</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.panFprep' + panFreshNum + '"></br>' + '<label>Special Instructions</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.panFspecial' + panFreshNum + '">');
         var $target = $('.panFreshTarget');
-        
+
         $list.append($label);
         $list.append($formItem);
         $target.append($list);
         panFreshNum = panFreshNum + 1;
       }
-      
-      function addPanDry(){
+
+      function addPanDry() {
         var $list = $('<li></li>');
         var title = 'Pantry Dry Ingredient ' + panDryNum;
         var $label = $('<label>' + title + '</label>');
-        var $formItem = $('<input class="form-control" type="text" ng-model="recipe.newRecipe.panDry' + panDryNum + '"></br>' + '<label>Amount</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.pDamount' + panDryNum + '"></br>' + '<label>Ingredient Prep</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.pDprep' + panDryNum + '"></br>' + '<label>Special Instructions</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.pDspecial' + panDryNum + '">');
+        var $formItem = $('<input class="form-control" type="text" ng-model="recipe.newRecipe.panDry' + panDryNum + '"></br>' + '<label>Amount</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.pDamount' + panDryNum + '"></br>' + '<label>Ingredient Prep</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.pDprep' + panDryNum + '"></br>' + '<label>Special Instructions</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.pDspecial' + panDryNum + '">');
         var $target = $('.panDryTarget');
-        
+
         $list.append($label);
         $list.append($formItem);
         $target.append($list);
         panDryNum = panDryNum + 1;
       }
-      
-      function addHerb(){
+
+      function addHerb() {
         var $list = $('<li></li>');
         var title = 'Herb &amp; Spices ' + herbNum;
         var $label = $('<label>' + title + '</label>');
-        var $formItem = $('<input class="form-control" type="text" ng-model="recipe.newRecipe.herb' + herbNum + '"></br>' + '<label>Amount</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.herbAmount' + herbNum + '"></br>' + '<label>Ingredient Prep</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.herbPrep' + herbNum + '"></br>' + '<label>Special Instructions</label>' 
-        + '<input class="form-control" type="text" ng-model="recipe.newRecipe.herbSpecial' + herbNum + '">');
+        var $formItem = $('<input class="form-control" type="text" ng-model="recipe.newRecipe.herb' + herbNum + '"></br>' + '<label>Amount</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.herbAmount' + herbNum + '"></br>' + '<label>Ingredient Prep</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.herbPrep' + herbNum + '"></br>' + '<label>Special Instructions</label>' + '<input class="form-control" type="text" ng-model="recipe.newRecipe.herbSpecial' + herbNum + '">');
         var $target = $('.herbTarget');
-        
+
         $list.append($label);
         $list.append($formItem);
         $target.append($list);
-        herbNum = herbNum + 1; 
+        herbNum = herbNum + 1;
       }
-      
-      function addDirection(){
+
+      function addDirection() {
         var $list = $('<li></li>');
         var title = 'Direction ' + dirNum;
         var $label = $('<label>' + title + '</label>');
         var $formItem = $('<input class="form-control" type="text" ng-model"recipe.newRecipe.direction' + dirNum + '">');
-        var $target = $('.directionTarget'); 
-        
+        var $target = $('.directionTarget');
+
         $list.append($label);
         $list.append($formItem);
         $target.append($list);
         dirNum = dirNum + 1;
       }
-      
-      vm.addRecipe = function(){
+
+
+      vm.addRecipe = function() {
         var name = vm.newRecipe.name;
-        $http.post('https://healthrecipes.firebaseio.com/' + encodeURI(name) + '.json', vm.newRecipe)
-        .success(function(data){
+        $http.post('https://healthrecipes.firebaseio.com/' + encodeURI(name) + '.json', vm.newRecipe).success(function(data) {
           vm.recipeItem[data.name] = vm.newRecipe;
-        }).error(function(err){
+        }).error(function(err) {
           alert('Recipe could not be added.');
         });
         vm.recipes.push(vm.newRecipe);
         vm.newRecipe = null;
       };
-      
-    });
 
+    });
 
   }());
