619928e4089db74fdc79a8b4d0f7525be9e5b4ac
thatknitter <kblewis.flt@gmail.com>
2015-01-05 10:05:09 -0600

    initial factory build

--- js/factories.js
+++ js/factories.js
@@ -13,6 +13,42 @@
 
       }
       
+      function getRecipe(name, cb){
+        $http.get(_recipeURL(name))
+        .success(function(data){
+          cb(data);
+        })
+        .error(function(err){
+          console.log(err);
+        });
+      }
+      
+      function listRecipes(cb){
+        $http.get(_recipeURL())
+        .success(function(data){
+          cb(data);
+        })
+        .error(function(err){
+          console.log(err);
+        });
+      }
+      
+      function deleteRecipe(nameId, cb){
+        $http.delete(_recipeURL(nameId))
+        .success(function(){
+          cb();
+        })
+        .error(function(err){
+          console.log(err);
+        });
+      }
+      
+      return{
+        getRecipe: getRecipe,
+        listRecipes: listRecipes,
+        deleteRecipe: deleteRecipe
+      };
+      
     });
   
   }());
